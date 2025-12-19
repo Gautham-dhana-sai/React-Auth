ddd79b9d2c83b409c3b3061114c5ebe35a978860
Pandiri Goutham Dhana Sai Reddy <gouthamdhanasai@Pandiris-MacBook-Air.local>
2025-12-19 23:38:28 +0530

    Final home page static design complete

--- src/components/Common/Item-Box.jsx
+++ src/components/Common/Item-Box.jsx
@@ -1,13 +1,23 @@
-const ItemBox = () => {
+import PropTypes from "prop-types";
+
+const ItemBox = ({item}) => {
   return (
     <>
       <div className="card-wrapper box">
         <div className="card">
-          <div className="placeholder1">Card 1</div>
+          <strong className="m-left-5 m-top-ne8">{item.title}</strong>
+          <div className="m-left-5 statement-block">{item.description}</div>
         </div>
       </div>
     </>
   );
 };
+
+ItemBox.propTypes = {
+  item: PropTypes.shape({
+    title: PropTypes.string,
+    description: PropTypes.string,
+  }),
+}
 
 export default ItemBox;
